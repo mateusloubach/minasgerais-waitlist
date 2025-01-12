397521e6e997766818c815e160bfa67898a906fb
mateusloubach <mcesarloubach@gmail.com>
2025-01-12 10:25:28 -0300

    chore: fix footer, insert repoLogo

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       initial="hidden"
       animate="visible"
       className="mt-auto flex w-full items-center justify-center gap-1 border-t bg-background p-6 text-muted-foreground md:justify-start">
-      <motion.div variants={itemVariants}>
+      {/* <motion.div variants={itemVariants}>
         Por{" "}
         <Link
           href="https://mateusloubach.tech"
@@ -20,9 +20,20 @@ export default function Footer() {
           </span>
           .
         </Link>
-      </motion.div>
-      <motion.div variants={itemVariants} className="ml-auto text-xs text-muted-foreground">
-        &copy; {new Date().getFullYear()} alternativaAgency. Todos os direitos reservados.
+      </motion.div> */}
+      <motion.div variants={itemVariants} className="ml-auto text-sm text-muted-foreground">
+        &copy; {new Date().getFullYear()}{" "}
+        
+        <Link
+          href="https://mateusloubach.tech"
+          rel="noopener noreferrer"
+          target="_blank">
+          <span className="text-zinc-300 underline underline-offset-2 transition-all duration-200 ease-linear hover:text-red-300">
+            mateusloubach
+          </span>
+        </Link>{" "}
+        
+        All rights reserved.
       </motion.div>
     </motion.div>
   );
